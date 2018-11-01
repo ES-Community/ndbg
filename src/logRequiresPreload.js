6dd461e3ab3ccb70afd3556d4ca2665411abf998
Gentilhomme <gentilhomme.thomas@gmail.com>
2018-11-01 10:15:48 +0100

    feat: trace-requires with perf_hooks api (#2)

--- src/logRequiresPreload.js
+++ src/logRequiresPreload.js
@@ -1,8 +1,25 @@
 'use strict';
 
-var js = require.extensions['.js'];
+const {
+  performance,
+  PerformanceObserver
+} = require('perf_hooks');
+const mod = require('module');
 
-require.extensions['.js'] = (...args) => {
-  console.log('loadingModule', args[0].filename);
-  return js(...args);
-};
+// Monkey patch the require function
+mod.Module.prototype.require = performance.timerify(mod.Module.prototype.require);
+require = performance.timerify(require);
+
+// Activate the perf observer
+const obs = new PerformanceObserver((list) => {
+  let fullRequireTime = 0;
+  const entries = list.getEntries();
+  entries.forEach((entry) => {
+    console.log(`loadingModule '${entry[0]}' in %s ms`, entry.duration);
+    fullRequireTime += entry.duration;
+  });
+  console.log(`Average require time: ${fullRequireTime / entries.length} ms`);
+  console.log(`Complete load time ${fullRequireTime} ms`);
+  obs.disconnect();
+});
+obs.observe({ entryTypes: ["function"], buffered: true });
